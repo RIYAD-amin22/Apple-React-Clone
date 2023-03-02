38121d2200f898196075a7a92ba9ce59b978fd22
Abdu <rigad2030@gmail.com>
2023-03-02 08:41:48 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import "./CommonResoucre/css/styles.css"
+import "./CommonResoucre/css/bootstrap.css"
+import Header from './Components/Header/Header';
+import Alert from './Components/Alert/Alert';
+import Main from './Components/Main/Main';
+import Footer from './Components/Footer/Footer';
+import YoutubeVideo from './Components/YoutubeVideo/YoutubeVideo';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Alert />
+      <Main />
+      <YoutubeVideo></YoutubeVideo>
+      <Footer/>
+
+
+
+
+      
     </div>
   );
 }
